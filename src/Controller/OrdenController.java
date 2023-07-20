37067bf88cc296649cb06617cafb7bf8b63b010c
Víctor López <vicren.lopez@outlook.com>
2023-07-20 09:57:39 -0600

    OrdenController init

--- src/Controller/OrdenController.java
+++ src/Controller/OrdenController.java
@@ -1,15 +1,24 @@
 package Controller;
 
 import Modelo.Orden;
+import Vista.frmComida;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class OrdenController implements ActionListener {
+
     private Orden orden;
+    frmComida frmComida = new frmComida();
+
+    public OrdenController(Orden orden, frmComida frmComida) {
+        this.orden = orden;
+        this.frmComida = frmComida;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
 
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Vista.frmComida;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
