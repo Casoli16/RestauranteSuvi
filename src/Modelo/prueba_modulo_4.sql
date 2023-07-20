2db3b067cc3586d36bb08fb5f9c3096eb0fcc33a
Víctor López <vicren.lopez@outlook.com>
2023-07-20 09:14:44 -0600

    Suvi revive

--- src/Modelo/prueba_modulo_4.sql
+++ src/Modelo/prueba_modulo_4.sql
@@ -1,3 +1,5 @@
+USE master;
+DROP DATABASE RestauranteSuvi;
 CREATE DATABASE RestauranteSuvi;
 USE RestauranteSuvi;
 
@@ -6,7 +8,8 @@ CREATE TABLE Orden
     id          INT IDENTITY PRIMARY KEY,
     nombre      VARCHAR(100),
     cantidad    INT,
-    precio      MONEY,
+    precio      DECIMAL,
     cliente     VARCHAR(100),
     observacion VARCHAR(500)
 );
+
